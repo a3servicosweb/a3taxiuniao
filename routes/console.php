a7b052b57412c9480a7eb4c6631e14277675d0f1
a3servicosweb <a3servicosweb@gmail.com>
2025-03-24 23:05:46 -0300

    Commit para ver se o Job 2

--- routes/console.php
+++ routes/console.php
@@ -4,13 +4,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-//Artisan::command('inspire', function () {
-//    $this->comment(Inspiring::quote());
-//})->purpose('Display an inspiring quote');
-//
-//Schedule::call(function () {
-//    \Illuminate\Support\Facades\Log::info('This is a scheduled task');
-//})->everyTenSeconds();
 
 Schedule::job(new \App\Jobs\UserCheckIdentityValidity())->everyMinute();
 
